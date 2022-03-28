55ad1533cca6340b514b9c4aba0600ef08dd23a2
carlosHenriquePires <carluxhenrique@gmail.com>
2022-03-27 21:44:08 -0300

    Acrescentando mais campos no model Team

--- godev_projeto_final/Models/Team.swift
+++ godev_projeto_final/Models/Team.swift
@@ -11,20 +11,27 @@ struct Team {
     var name: String
     var ocupation: String
     var image: String
+    var fone: String
+    var email: String
+    var linkedin: String
     
-    init(name: String, ocupation: String, image: String) {
+    
+    init(name: String, ocupation: String, image: String, fone: String, email:String, linkedin:String) {
         self.name = name
         self.ocupation = ocupation
         self.image = image
+        self.fone = fone
+        self.email = email
+        self.linkedin = linkedin
     }
     
     static func populateData() -> [Team]{
         let components:[Team] = [
-            Team(name: "Carlos Henrique", ocupation: "Developer Mobile", image: "carlos"),
-            Team(name: "Carlos Henrique", ocupation: "Developer Mobile", image: "carlos"),
-            Team(name: "Carlos Henrique", ocupation: "Developer Mobile", image: "carlos"),
-            Team(name: "Carlos Henrique", ocupation: "Developer Mobile", image: "carlos"),
-            Team(name: "Carlos Henrique", ocupation: "Developer Mobile", image: "carlos")]
+            Team(name: "Carlos Henrique", ocupation: "Developer Mobile", image: "carlos", fone: "(84) 98715-0582", email: "carluxhenrique@gmail.com", linkedin: "https://www.linkedin.com/in/carlos-henrique-pires-dos-santos-23251a122/"),
+            Team(name: "Carlos Henrique", ocupation: "Developer Mobile", image: "carlos", fone: "(84) 98715-0582", email: "carluxhenrique@gmail.com", linkedin: "https://www.linkedin.com/in/carlos-henrique-pires-dos-santos-23251a122/"),
+            Team(name: "Carlos Henrique", ocupation: "Developer Mobile", image: "carlos", fone: "(84) 98715-0582", email: "carluxhenrique@gmail.com", linkedin: "https://www.linkedin.com/in/carlos-henrique-pires-dos-santos-23251a122/"),
+            Team(name: "Carlos Henrique", ocupation: "Developer Mobile", image: "carlos", fone: "(84) 98715-0582", email: "carluxhenrique@gmail.com", linkedin: "https://www.linkedin.com/in/carlos-henrique-pires-dos-santos-23251a122/"),
+            Team(name: "Carlos Henrique", ocupation: "Developer Mobile", image: "carlos", fone: "(84) 98715-0582", email: "carluxhenrique@gmail.com", linkedin: "https://www.linkedin.com/in/carlos-henrique-pires-dos-santos-23251a122/")]
         
         return components
     }
